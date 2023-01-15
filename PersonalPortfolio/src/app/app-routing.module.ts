9d64ce51048416501d459c4097b78a3cc7a9de7b
[PykeMG] <[joseandree.j42@gmail.com]>
2023-01-15 01:08:47 -0500

    Addead all the components and the redirect links, also the new titles for any component into de project

--- PersonalPortfolio/src/app/app-routing.module.ts
+++ PersonalPortfolio/src/app/app-routing.module.ts
@@ -3,23 +3,47 @@ import { RouterModule, Routes } from '@angular/router';
 import { CoverComponent } from './components/cover/cover.component';
 import { AboutComponent } from './components/about/about.component';
 import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { WorkComponent } from './components/work/work.component';
 const routes: Routes = [
-  {
-    path: '',
-    component: CoverComponent,
-  },
   {
     path: 'cover',
     component: CoverComponent,
+    title: 'Cover',
   },
   {
     path: 'about',
     component: AboutComponent,
+    title: 'About Me',
   },
   {
     path: 'education',
     component: EducationComponent,
+    title: 'Education',
+  },
+  {
+    path: 'experience',
+    component: ExperienceComponent,
+    title: 'Work Experience',
+  },
+  {
+    path: 'work',
+    component: WorkComponent,
+    title: 'My Work',
+  },
+  {
+    path: '**',
+    redirectTo: '/cover', pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: CoverComponent,
   }
+  // ,
+  // {
+  //   path: '**',
+  //   component: CoverComponent,
+  // }
 ];
 
 @NgModule({
--- PersonalPortfolio/src/app/app.module.ts
+++ PersonalPortfolio/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -8,6 +8,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { CoverComponent } from './components/cover/cover.component';
 import { AboutComponent } from './components/about/about.component';
 import { EducationComponent } from './components/education/education.component';
+import { WorkComponent } from './components/work/work.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { EducationComponent } from './components/education/education.component';
     FooterComponent,
     CoverComponent,
     AboutComponent,
-    EducationComponent
+    EducationComponent,
+    WorkComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
